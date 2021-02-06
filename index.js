0b8f016319f5b2c709fd6e4f55596c04086a39db
Greg Lincoln <greg@greglincoln.com>
2021-02-06 07:18:35 -0500

    Use async/await.

--- index.js
+++ index.js
@@ -4,49 +4,38 @@ const dboper = require('./operations');
 const url = 'mongodb://localhost:27017/';
 const dbname = 'nucampsite';
 
-MongoClient.connect(url, { useUnifiedTopology: true }).then(client => {
-
+(async () => {
+    const client = await MongoClient.connect(url, { useUnifiedTopology: true });
     console.log('Connected correctly to server');
 
     const db = client.db(dbname);
+    try {
+        let result = await db.dropCollection('campsites');
+        console.log('Dropped Collection:', result);
+    }
+    catch {
+        console.log('Collection not found, skipping drop.');
+    }
 
-    db.dropCollection('campsites')
-        .then(result => {
-            console.log('Dropped Collection:', result);
-        })
-        .catch(err => console.log('No collection to drop.'));
+    try {
+        let result = await dboper.insertDocument(db, { name: "Breadcrumb Trail Campground", description: "Test" }, 'campsites');
+        console.log('Insert Document:', result.ops);
 
-    dboper.insertDocument(db, { name: "Breadcrumb Trail Campground", description: "Test" }, 'campsites')
-        .then(result => {
-            console.log('Insert Document:', result.ops);
+        let docs = await dboper.findDocuments(db, 'campsites');
+        console.log('Found Documents:', docs);
 
-            return dboper.findDocuments(db, 'campsites');
-        })
-        .then(docs => {
-            console.log('Found Documents:', docs);
+        result = await dboper.updateDocument(db, { name: "Breadcrumb Trail Campground" },
+            { description: "Updated Test Description" }, 'campsites');
+        console.log('Updated Document Count:', result.result.nModified);
 
-            return dboper.updateDocument(db, { name: "Breadcrumb Trail Campground" },
-                { description: "Updated Test Description" }, 'campsites');
-        })
-        .then(result => {
-            console.log('Updated Document Count:', result.result.nModified);
+        docs = await dboper.findDocuments(db, 'campsites');
+        console.log('Found Documents:', docs);
 
-            return dboper.findDocuments(db, 'campsites');
-        })
-        .then(docs => {
-            console.log('Found Documents:', docs);
+        result = await dboper.removeDocument(db, { name: "Breadcrumb Trail Campground" }, 'campsites');
+        console.log('Deleted Document Count:', result.deletedCount);
 
-            return dboper.removeDocument(db, { name: "Breadcrumb Trail Campground" },
-                'campsites');
-        })
-        .then(result => {
-            console.log('Deleted Document Count:', result.deletedCount);
-
-            return client.close();
-        })
-        .catch(err => {
-            console.log(err);
-            client.close();
-        });
-}).catch(err => console.log(err));
-
+        await client.close();
+    } catch (error) {
+        console.log('Unexpected error:', error)
+    }
+})();
